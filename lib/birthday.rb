d66ef86b1523497c2a00b9b828758dc7faca7c85
Joanna E K <kalemajoanna@gmail.com>
2019-12-12 09:56:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,5 +14,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   if birthday_kids >= 12 
-    
+    birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 end 
